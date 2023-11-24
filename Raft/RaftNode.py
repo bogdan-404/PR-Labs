a84bf7342e9afc0387302f675e2ba507f35d99db
bogdan-404 <bogdan.zlatovcen@gmail.com>
2023-11-24 12:44:13 +0200

    Correct the follower-leaders election

--- Raft/RaftNode.py
+++ Raft/RaftNode.py
@@ -1,53 +1,21 @@
-import socket
-import threading
-import time
-import random
-import requests
+import os
+import logging
 
 
 class RaftNode:
-    def __init__(self, node_id, peers):
+    def __init__(self, node_id, lock_file_path):
         self.node_id = node_id
         self.state = "follower"
-        self.peers = peers
-        self.start_election_thread()
-        self.follower_credentials = {}
+        self.lock_file_path = lock_file_path
+        self.elect_leader()
 
-    def start_election_thread(self):
-        election_thread = threading.Thread(target=self.run_election_cycle)
-        election_thread.daemon = True
-        election_thread.start()
-
-    def run_election_cycle(self):
-        while True:
-            time.sleep(random.uniform(5, 10))
-            if self.state != "leader":
-                self.start_election()
-
-    def start_election(self):
-        self.state = "candidate"
-        election_port = 10000
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def elect_leader(self):
         try:
-            udp_socket.bind(("", election_port))
+            with open(self.lock_file_path, "x") as lock_file:
+                lock_file.write(self.node_id)
             self.state = "leader"
-        except socket.error:
-            self.state = "follower"
-        finally:
-            udp_socket.close()
-
-    def update_follower_list(self, follower_address):
-        if follower_address not in self.follower_credentials:
-            self.follower_credentials[follower_address] = {
-                "ip": follower_address.split(":")[0],
-                "port": follower_address.split(":")[1],
-            }
-
-    def forward_request_to_followers(self, endpoint, data):
-        if self.state == "leader":
-            for follower in self.follower_credentials.values():
-                try:
-                    follower_address = f"{follower['ip']}:{follower['port']}"
-                    requests.post(f"http://{follower_address}{endpoint}", json=data)
-                except requests.RequestException:
-                    pass
+        except FileExistsError:
+            with open(self.lock_file_path, "r") as lock_file:
+                leader_id = lock_file.read()
+            self.state = "leader" if leader_id == self.node_id else "follower"
+        logging.info(f"Node {self.node_id} initialized as {self.state}")
--- Raft/app.py
+++ Raft/app.py
@@ -2,34 +2,29 @@ from flask import Flask, request, jsonify
 from models import db, ElectroScooter
 from RaftNode import RaftNode
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = f"sqlite:///database_{os.getenv('NODE_ID', 'node_1')}.db"
+node_id = os.getenv("NODE_ID", "node_1")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///database_{node_id}.db"
 db.init_app(app)
 
-node_id = os.getenv("NODE_ID", "node_1")
-peers = os.getenv("PEERS", "localhost:5001,localhost:5002").split(",")
-
-raft_node = RaftNode(node_id, peers)
-
-
-def init_db():
-    with app.app_context():
-        db.create_all()
+lock_file_path = "raft_leader.lock"
+raft_node = RaftNode(node_id, lock_file_path)
 
 
 @app.route("/api/electro-scooters/", methods=["POST"])
 def create_electro_scooter():
     if raft_node.state != "leader":
         return jsonify({"error": "Not the leader"}), 403
+
     data = request.get_json()
     new_scooter = ElectroScooter(name=data["name"], battery_level=data["battery_level"])
     db.session.add(new_scooter)
     db.session.commit()
-    raft_node.forward_request_to_followers("/api/electro-scooters/", data)
-    return jsonify(new_scooter.id), 201
+    return jsonify({"id": new_scooter.id}), 201
 
 
 @app.route("/api/electro-scooters/<int:scooter_id>", methods=["GET"])
@@ -67,7 +62,14 @@ def update_electro_scooter(scooter_id):
 @app.route("/api/electro-scooters/<int:scooter_id>", methods=["DELETE"])
 def delete_electro_scooter(scooter_id):
     if raft_node.state != "leader":
-        return jsonify({"error": "Not the leader"}), 403
+        return (
+            jsonify(
+                {
+                    "error": "It is a follower. Only leader has privileges to the request."
+                }
+            ),
+            403,
+        )
     scooter = ElectroScooter.query.get(scooter_id)
     if not scooter:
         return jsonify({"error": "Scooter not found"}), 404
@@ -77,10 +79,10 @@ def delete_electro_scooter(scooter_id):
     return jsonify({"message": "Deleted"}), 200
 
 
-# if __name__ == "__main__":
-#     db.create_all()
-#     port = int(os.getenv("PORT", 5000))
-#     app.run(port=port, debug=True)
+def init_db():
+    with app.app_context():
+        db.create_all()
+
 
 if __name__ == "__main__":
     init_db()
